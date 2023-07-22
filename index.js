b4fad414cb66331409dd5d09091c3c43bc9db5b5
munir79 <jakirhossainmunir79@gmail.com>
2023-07-22 11:18:34 +0600

    add middleware

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require('express');
 const cors =require('cors');
 const app=express();
 const port=process.env.PORT || 5000;
+ // middle ware
+app.use(cors());
+app.use(express.json());
+
  app.get('/',(req,res)=>{
     res.send('hello from node mongo server ');
  })
