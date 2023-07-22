4360037731ac05b04dfcfc0bbb07472192bb55eb
munir79 <jakirhossainmunir79@gmail.com>
2023-07-22 11:03:24 +0600

    server starting code

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const cors =require('cors');
+const app=express();
+const port=process.env.PORT || 5000;
+ app.get('/',(req,res)=>{
+    res.send('hello from node mongo server ');
+ })
+
+ app.listen(port ,()=>{
+    console.log(`listening to port ${port} `);
+ })
